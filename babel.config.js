4738f3a31a3854bb9dc36338db8c2026bc347f56
strongsun806 <145644915+strongsun806@users.noreply.github.com>
2025-03-06 14:29:59 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',  // 애니메이션 최적화
+      '@babel/plugin-proposal-export-namespace-from',  // ESNext 기능 지원
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './assets',
+        },
+      }],
+    ],
   };
 };
